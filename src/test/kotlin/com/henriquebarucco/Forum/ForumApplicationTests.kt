29b1ed45dc73b8fc4b81c0b92257b8b9fdcecf26
Henrique Barucco <henrique_barucco@hotmail.com>
2023-11-27 14:45:48 -0300

    first commit

---
+++ src/test/kotlin/com/henriquebarucco/Forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.henriquebarucco.Forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
